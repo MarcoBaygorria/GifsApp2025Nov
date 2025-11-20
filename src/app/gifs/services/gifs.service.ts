d500bd3010b1efb20ad8d730db63763035f96573
MarcoBaygorria <baygorria91@gmail.com>
2025-11-19 21:31:52 -1100

    peticionHTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,18 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interface';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
 
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
     private http = inject(HttpClient);
+
+    trendingGifs = signal<Gif[]>([]);
+    trendingGifsLoading = signal(true)
 
     constructor() {
         this.loadTrendingGifs();
@@ -19,6 +24,11 @@ export class GifService {
                 api_key: environment.giphyApiKey,
                 limit: 20,
             }
+        }).subscribe((resp) => {
+            const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data)
+            this.trendingGifs.set(gifs);
+            this.trendingGifsLoading.set(false)
+            console.log({ gifs })
         })
     }
 }
